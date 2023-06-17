8ebf961f17e4c3b102d0fbda659d1f54ce628dad
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-06-17 10:59:53 -0400

    app skeleton with buttons and dropdown

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-//comment
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Table from './components/Table';
 
 function App() {
+  const [grid, setGrid] = useState([[]]);
+  const [currentColor, setCurrentColor] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ColorPicker</h1>
+
+      <button>Add Row</button>
+      <button>Add Column</button>
+      <button>Remove Row</button>
+      <button>Remove Column</button>
+      <button>Color All</button>
+      <button>Clear All</button>
+
+      <h2>Pick a color and change the color of a cell!</h2>
+
+      <select>
+        <option value="choose" selected disabled>Choose a color</option>
+        <option>Red</option>
+        <option>Yellow</option>
+        <option>Green</option>
+        <option>Blue</option>
+        <option>Purple</option>
+        <option>Orange</option>
+      </select>
+      <Table />
     </div>
   );
 }
---
+++ src/components/TableRow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TableCell from './TableCell'
+
+function TableRow() {
+  return (
+    <div>TableRow</div>
+  )
+}
+
+export default TableRow
